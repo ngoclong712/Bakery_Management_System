666065b7b51363331714017e849204ba08cbc3b5
ngoclong712 <vanngoclong2003@gmail.com>
2024-04-06 23:07:16 +0700

    build: build frame for bakery management system

---
+++ bakery/admin/connect.php
@@ -0,0 +1,4 @@
+<?php 
+
+$connect = mysqli_connect('localhost','root','123456','bakery');
+mysqli_set_charset($connect, 'utf8');
---
+++ bakery/admin/menu.php
@@ -0,0 +1,21 @@
+<ul>
+	<li>
+		<a href="../manufacturers">
+			Quản lý nhà sản xuất
+		</a>
+	</li>
+</ul>
+<ul>
+	<li>
+		<a href="../products">
+			Quản lý sản phẩm
+		</a>
+	</li>
+</ul>
+<ul>
+	<li>
+		<a href="../orders">
+			Quản lý đơn hàng
+		</a>
+	</li>
+</ul>

27920436ed42e0ce33405c959668a8a1dbb7768d
ngoclong712 <vanngoclong2003@gmail.com>
2024-04-07 14:50:17 +0700

    fix: fix validation insert manufacturer
    Could blank image

--- bakery/admin/manufacturers/process_insert.php
+++ bakery/admin/manufacturers/process_insert.php
@@ -4,25 +4,32 @@ session_start();
 include '../check_super_admin_login.php';
 include '../connect.php';
 
-$photo = $_FILES['photo'];
-$file_extension = explode('.', $photo['name'])[1];
 
 
-if(empty($_POST['name']) ||empty($_POST['address']) || empty($_POST['phone']) || empty($file_extension)) {
+
+
+if(empty($_POST['name']) ||empty($_POST['address']) || empty($_POST['phone'])) {
 	header('location:form_insert.php');
 	$_SESSION['error'] = "Phải điền đầy đủ thông tin";
 	exit;
 }
 
+$photo = $_FILES['photo'];
 $name = $_POST['name'];
 $address = $_POST['address'];
 $phone = $_POST['phone'];
 
-$folder = "photos/";
-$file_name = time(). '.' . $file_extension;
-$file_path = $folder.$file_name;
+$file_extension = explode('.', $photo['name'])[1];
+if(empty($file_extension)) {
+	$file_name = "current_empty";
+}
+else {
+	$folder = "photos/";
+	$file_name = time(). '.' . $file_extension;
+	$file_path = $folder.$file_name;
+	move_uploaded_file($photo['tmp_name'], $file_path);
+}
 
-move_uploaded_file($photo['tmp_name'], $file_path);
 
 $sql = "insert into manufacturers(name, address, photo, phone)
 values('$name','$address','$file_name','$phone')";

fc6f55da271d3176197a63bc0efc7024ae8764bb
ngoclong712 <vanngoclong2003@gmail.com>
2024-05-03 11:04:57 +0700

    upd: update file test.php

--- bakery/test.php
+++ bakery/test.php
@@ -11,6 +11,28 @@ Chọn thời gian
 <input type="week" name="">
 <input type="month" name="">
 <!-- 
+đóng kết nối ở đâu (tránh trong vòng lặp)
+
+tạo file header rồi include, k chèn lại menu
+
+*admin: nên để table, để thẳng hàng cho sửa xóa, không để div
+
+làm tìm kiếm, phân trang, phân trang nên tống 1 file riêng
+
+dung PDO kết hợp prepare param tránh sql injection
+
+theo 1 quy tắc, vd viết hoa chữ cái đầu cho tiêu đề =>> áp dụng hết
+
+validate trước =>> r mới khai báo biến, validate có thể addslashes
+
+nút đăng nhập ở bên trái, hủy ở bên phải
+
+nên dùng exit() thay vì die(), vì die() dùng để hiển thị lỗi
+
+*sản phẩm k được phép xóa nếu nằm trong hóa đơn
+
+super admin không được phép xóa nhau, k được phép xóa bản thân, admin không được phép xóa
+
 select option chạy vòng for từ min tới date('Y') 
 số sản phẩm đã đặt: left join group by ten
 SELECT
